0deb873cad5d1fb0e1cdff3fc88e9a55a1ea9258
ichater <chaterizaak@gmail.com>
2020-04-10 16:36:00 +1000

    understanding types

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(phrase, n1, n2, showResult) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var number1 = 5;
-var number2 = "5.6";
-var result = add(number1, number2);
+var number2 = 15;
+var printResult = true;
+var resultPhrase = "result is: ";
+var result = add(resultPhrase, number1, number2, printResult);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,8 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(phrase: string, n1: number, n2: number, showResult: boolean) {
+  let result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
-const number2 = 5.6;
-const result = add(number1, number2);
+const number2 = 15;
+const printResult = true;
+const resultPhrase = "result is: ";
+
+const result = add(resultPhrase, number1, number2, printResult);
 console.log(result);
