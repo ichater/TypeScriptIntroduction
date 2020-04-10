ff96eea58e104bdb07776cd2901744e0277e924f
ichater <chaterizaak@gmail.com>
2020-04-10 13:06:20 +1000

    set up dev ennvironment and server-lite

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 5.6;
+const result = add(number1, number2);
+console.log(result);
